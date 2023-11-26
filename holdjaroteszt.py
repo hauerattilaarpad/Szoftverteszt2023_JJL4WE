c6ebbcb908e340ebe29fe6f1a57dbf378791068b
hauerattilaarpad <90259862+hauerattilaarpad@users.noreply.github.com>
2023-11-26 16:31:46 +0100

    elore kelet

---
+++ holdjaroteszt.py
@@ -0,0 +1,30 @@
+import unittest
+
+from Holdjaro import holdjaro
+
+
+class holdjaroteszt(unittest.TestCase):
+
+  def test_forward_north(self):
+
+    rover = holdjaro(0, 0, 'N')
+
+    result = rover.lepes('f')
+
+    self.assertEqual(result, (0, 1, 'N'))
+
+  def test_forward_south(self):
+    rover = holdjaro(0, 0, 'S')
+    result = rover.lepes('f')
+    self.assertEqual(result, (0, -1, 'S'))
+
+  def test_forward_east(self):
+    rover = holdjaro(0, 0, 'E')
+    result = rover.lepes('f')
+    self.assertEqual(result, (1, 0, 'E'))
+
+
+
+if __name__ == '__main__':
+
+  unittest.main()
