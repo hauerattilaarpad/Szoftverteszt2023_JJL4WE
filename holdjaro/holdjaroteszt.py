fe7e83636f6a653a797d1894ba15a69eab89c7e8
hauerattilaarpad <90259862+hauerattilaarpad@users.noreply.github.com>
2023-11-26 16:47:49 +0100

    összes teszteset újracsinálása hisz az akadály miatt elromlott. valamint akadály teszt

--- holdjaro/holdjaroteszt.py
+++ holdjaro/holdjaroteszt.py
@@ -6,43 +6,46 @@ from Holdjaro import holdjaro
 class holdjaroteszt(unittest.TestCase):
 
   def test_forward_north(self):
-
-    rover = holdjaro(0, 0, 'N')
-
+    rover = holdjaro(0, 0, 'N', [])
     result = rover.lepes('f')
-
     self.assertEqual(result, (0, 1, 'N'))
 
   def test_forward_south(self):
-    rover = holdjaro(0, 0, 'S')
+    rover = holdjaro(0, 0, 'S', [])
     result = rover.lepes('f')
     self.assertEqual(result, (0, -1, 'S'))
 
   def test_forward_east(self):
-    rover = holdjaro(0, 0, 'E')
+    rover = holdjaro(0, 0, 'E', [])
     result = rover.lepes('f')
     self.assertEqual(result, (1, 0, 'E'))
 
   def test_forward_west(self):
-    rover = holdjaro(0, 0, 'W')
+    rover = holdjaro(0, 0, 'W', [])
     result = rover.lepes('f')
     self.assertEqual(result, (-1, 0, 'W'))
 
-  def test_right(self):
-    rover = holdjaro(0, 0, 'E')
-    result = rover.lepes('r')
-    self.assertEqual(result, (0, 0, 'S'))
+  def test_backward(self):
+    rover = holdjaro(0, 0, 'E', [])
+    result = rover.lepes('b')
+    self.assertEqual(result, (1, 0, 'E'))
 
   def test_left(self):
-    rover = holdjaro(0, 0, 'N')
+    rover = holdjaro(0, 0, 'N', [])
     result = rover.lepes('l')
     self.assertEqual(result, (0, 0, 'W'))
 
-  def test_backward(self):
-    rover = holdjaro(0, 0, 'E')
-    result = rover.lepes('b')
-    self.assertEqual(result, (1, 0, 'E'))
-      
+  def test_right(self):
+    rover = holdjaro(0, 0, 'E', [])
+    result = rover.lepes('r')
+    self.assertEqual(result, (0, 0, 'S'))
+
+  def test_obstacle(self):
+    rover = holdjaro(0, 0, 'N', [(0, 1)])
+    result = rover.lepes('f')
+    self.assertEqual(result, "Vigyázz akadály")
+
+
 if __name__ == '__main__':
 
   unittest.main()
